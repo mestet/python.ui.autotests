ca4d2de5e32d49898c34072446bc86b3ddb62225
m.vlasov <m.vlasov@lucky-teams.pro>
2023-01-19 00:21:45 +0100

    explicit wait for element

--- tests/test_login_smoke.py
+++ tests/test_login_smoke.py
@@ -1,5 +1,7 @@
 import allure
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 from utils.page import find_element
 
@@ -14,18 +16,19 @@ def test_login_smoke(chrome_browser):
     assert chrome_browser.current_url.startswith("https://my.luckyfeed.pro/login")
 
 
-# @allure.testcase("LuckyFeed login button")
-# def test_login_form(chrome_browser, autotest_user):
-#     chrome_browser.get("https://my.luckyfeed.pro/login")
-#     login_field = find_element(chrome_browser, 'xpath', '//input[@type="email"]')
-#     password_field = find_element(chrome_browser, 'xpath', '//input[@type="password"]')
-#     submit_button = find_element(chrome_browser, 'xpath', '//button[@type="submit"]')
-#
-#     # todo parametrize
-#     login_field.send_keys("autotest@lucky-team.pro")
-#     password_field.send_keys("d2f0fd3eef")
-#     submit_button.click()
-#     time.sleep(10)  # todo do not wait or wait less ex WebDriverWait(driver, 30).until()
-#
-#     user_email = find_element(chrome_browser, 'xpath', '//div[@class="lna-profile__avatar-email"]')
-#     assert user_email.text == 'autotest@lucky-team.pro'
+@allure.testcase("LuckyFeed login button")
+def test_login_form(chrome_browser, autotest_user):
+    chrome_browser.get("https://my.luckyfeed.pro/login")
+    login_field = find_element(chrome_browser, By.XPATH, '//input[@type="email"]')
+    password_field = find_element(chrome_browser, By.XPATH, '//input[@type="password"]')
+    submit_button = find_element(chrome_browser, By.XPATH, '//button[@type="submit"]')
+
+    # todo parametrize
+    login_field.send_keys("autotest@lucky-team.pro")
+    password_field.send_keys("d2f0fd3eef")
+    submit_button.click()
+    user_email = WebDriverWait(chrome_browser, 30).until(
+        expected_conditions.presence_of_element_located((By.XPATH, '//div[@class="lna-profile__avatar-email"]'))
+    )
+
+    assert user_email.text == 'autotest@lucky-team.pro'
